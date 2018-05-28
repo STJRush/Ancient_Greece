61a340d555c4e9db8c921fab6f324f7855ea295a
PDGGAMER <16PWhyte@stjosephsrush.ie>
2018-05-28 06:55:28 +0100

    Update tilemap.py

--- tilemap.py
+++ tilemap.py
@@ -1,20 +1,46 @@
 import pygame as pg
+import os
+import pytmx
 from settings import *
+from pytmx.util_pygame import load_pygame
 
 def collide_hit_rect(one, two):
     return one.hit_rect.colliderect(two.rect)
-    
+
 class Map:
     def __init__(self, filename):
         self.data = []
         with open(filename, 'rt') as f:
-            for line in  f:
+            for line in f:
                 self.data.append(line.strip())
 
         self.tilewidth = len(self.data[0])
         self.tileheight = len(self.data)
         self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
+
+#class TiledMap:
+    #def __init__(self, filename):
+        #tm = load_pygame(filename, pixelalpha=True)
+        #self.width = tm.width * tm.tilewidth
+        #self.height = tm.height * tm.tileheight
+        #self.tmxdata = tm
+
+    #def render(self, surface):
+        #ti = self.tmxdata.get_tile_image_by_gid
+        #for layer in self.tmxdata.visible_layers:
+            #if isinstance(layer, pytmx.TiledTileLayer):
+                #for x, y, gid, in layer:
+                    #tile = ti(gid)
+                    #if tile:
+                        #surface.blit(tile, (x * self.tmxdata.tilewidth,
+                                            #y * self.tmxdata.tileheight))
+
+    #def make_map(self):
+        #temp_surface = pg.Surface((self.width, self.height))
+        #self.render(temp_surface)
+        #return temp_surface
+
 
 class Camera:
     def __init__(self, width, height):
@@ -24,6 +50,9 @@ class Camera:
 
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
+
+    def apply_rect(self, rect):
+        return rect.move(self.camera.topleft)
 
     def update(self, target):
         x = -target.rect.centerx + int(WIDTH / 2)
